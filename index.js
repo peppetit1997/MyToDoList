5d4d5c0f4e2f2b3581d31510ce14afabca133060
pierre-Etienne Petit <pierre-etienne.petit@hotmail.com>
2023-09-03 13:10:09 -0400

    Minimal feature of a to do list

--- index.js
+++ index.js
@@ -6,11 +6,22 @@ const port = 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true })); // Parse POST request data
+
+let tasks = []; // Array to store tasks
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index", { tasks }); // Pass tasks array to the template
+});
+
+app.post("/addTask", (req, res) => {
+  const newTask = req.body.taskToAdd; // Get the task from the form data
+  if (newTask) {
+    tasks.push(newTask); // Add the task to the tasks array
+  }
+  res.redirect("/"); // Redirect back to the homepage
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
--- public/script.js
+++ public/script.js
@@ -1,12 +1,31 @@
-const changeButton = document.getElementById("changeButton");
+const addTaskButton = document.getElementById("addTaskButton");
+const taskInput = document.getElementById("taskToAdd");
+const taskList = document.getElementById("taskList");
 
-changeButton.addEventListener("click", () => {
-  fetch("/changeHeader")
-    .then((response) => response.json())
-    .then((data) => {
-      document.querySelector("h1").textContent = data.newHeader;
+addTaskButton.addEventListener("click", () => {
+  const taskText = taskInput.value.trim();
+
+  if (taskText !== "") {
+    // Send a POST request to add the task
+    fetch("/addTask", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ taskToAdd: taskText }),
     })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        // Add the task to the taskList
+        const listItem = document.createElement("li");
+        listItem.textContent = data.newTask;
+        taskList.appendChild(listItem);
+
+        // Clear the input field
+        taskInput.value = "";
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 });
