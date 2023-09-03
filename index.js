1df99f1a899b8bd69ae6620fb2e44ac6162a7cdb
pierre-Etienne Petit <pierre-etienne.petit@hotmail.com>
2023-09-03 12:17:33 -0400

    More dependencies stuff

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+const app = express(); // Create an Express application instance
+const port = 3000;
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
