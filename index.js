4c5b655341640966e4605aa0b6c877a9326d63d8
pierre-Etienne Petit <pierre-etienne.petit@hotmail.com>
2023-09-03 11:47:17 -0400

    Installing more dependancies

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
